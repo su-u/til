f265cf13eb53b670a790c01244e9c3bdf9eea547
su- <su-.dev@unitech.jp>
2022-03-31 08:18:45 +0000

    a

--- package_sample/go_uber_mutilierr/mai.go
+++ package_sample/go_uber_mutilierr/mai.go
@@ -1,24 +1,3 @@
-// package main
-
-// import (
-// 	"errors"
-// 	"fmt"
-
-// 	"go.uber.org/multierr"
-// )
-
-// func main() {
-// 	err := multierr.Combine(
-// 		errors.New("call 1 failed"),
-// 		nil,
-// 		errors.New("call 2 failed"),
-// 		// nil,
-// 		errors.New("call 3 failed"),
-// 	)
-
-// 	fmt.Printf("%+v", err)
-// }
-
 package main
 
 import (
@@ -31,10 +10,11 @@ import (
 func main() {
 	err := multierr.Combine(
 		errors.New("call 1 failed"),
-		nil, // successful request
+		nil,
+		errors.New("call 2 failed"),
+		// nil,
 		errors.New("call 3 failed"),
-		nil, // successful request
-		errors.New("call 5 failed"),
 	)
+
 	fmt.Printf("%+v", err)
 }
