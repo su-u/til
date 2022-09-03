6f18d7b678c9f41fc4fe1d1a66e6b990d226d2ec
su- <happy.oil2469@fastmail.com>
2022-09-03 13:14:52 +0900

    pt-query-digest

--- mysql/percona-toolkit/workspace/console.sql
+++ mysql/percona-toolkit/workspace/console.sql
@@ -2,12 +2,18 @@ show variables like 'slow_query%';
 
 show variables like 'long%';
 
-set global slow_query_log_file='/tmp/slow.log';
 set global slow_query_log=1;
+set global slow_query_log_file='/tmp/slow.log';
 set global long_query_time=1;
 
 
 SELECT SLEEP(3);
 
+SELECT SLEEP(10);
+
+SELECT SLEEP(0.5);
+
 SELECT * FROM address
-WHERE address2  IS NOT NULL
+WHERE address2  IS NOT NULL;
+
+SHOW GLOBAL VARIABLES;
