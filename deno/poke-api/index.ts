b75af7987c02058f087d08efe07382db6b202d93
su- <30166652+su-u@users.noreply.github.com>
2023-07-22 23:15:02 +0900

    deno api

--- deno/poke-api/index.ts
+++ deno/poke-api/index.ts
@@ -1,17 +1,30 @@
-import { serve } from "https://deno.land/std@0.157.0/http/server";
+import { serve } from "https://deno.land/std@0.157.0/http/server.ts";
 
 const handler = async (request: Request): Promise<Response> => {
-  const url = "https://pokeapi.co/api/v2/pokemon/pikachu/";
+  const requestUrl = new URL(request.url);
+  const pokemonNumber = requestUrl.searchParams.get("number");
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`;
   const resp = await fetch(url, {
     headers: {
       accept: "application/json",
     },
   });
 
-  return new Response(resp.body, {
+  let body = `<p>Not Found. number: ${pokemonNumber}`;
+  if (resp.status === 200) {
+    const json = await resp.json();
+    body = `
+      <p>Id: ${json["id"]}</p>
+      <p>Name: ${json["name"]}</p>
+      <img src="${json["sprites"]["front_default"]}" alt="${json["name"]}">
+    `;
+  }
+
+  return new Response(body, {
     status: resp.status,
     headers: {
-      "content-type": "application/json",
+      "content-type": "text/html",
     },
   });
 };
--- deno/poke-api/main.mk
+++ deno/poke-api/main.mk
@@ -1,5 +1,5 @@
 start:
-	deno run --allow-net index.ts
+	deno run --allow-net --watch index.ts
 
 curl:
-	curl localhost:8000 | jq
+	curl localhost:8000?number=1
