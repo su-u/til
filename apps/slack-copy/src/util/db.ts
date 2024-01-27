e3ab0daa84d95367e82ef6d9243cbdcda97d4711
su-u <happy.oil2469@fastmail.com>
2024-01-27 12:30:21 +0900

    DB schema

--- apps/slack-copy/src/util/db.ts
+++ apps/slack-copy/src/util/db.ts
@@ -1,3 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
+import type { Database } from '@/schema';
 
-export const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
+export const supabase = createClient<Database>(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY);
